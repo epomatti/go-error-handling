16326869db95b71677b38b57342397a02b886b30
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-04-27 11:11:59 -0300

    table-driven testing

--- main_test.go
+++ main_test.go
@@ -40,3 +40,39 @@ func Test_XBiggerThanY(t *testing.T) {
 		t.Errorf("Error = '%v', want nil", err)
 	}
 }
+
+func Test_Calc(t *testing.T) {
+	tests := []struct {
+		X   int
+		Y   int
+		Res int
+		Err string
+	}{
+		{
+			X: 1, Y: -1, Res: 0, Err: "y > 0",
+		},
+		{
+			X: 1, Y: 1, Res: 0, Err: "x > y",
+		},
+		{
+			X: 5, Y: 2, Res: 7, Err: "",
+		},
+	}
+
+	for _, test := range tests {
+		res, err := Calc(test.X, test.Y)
+
+		if res != test.Res {
+			t.Errorf("Result = %d, want %d", res, test.Res)
+		}
+
+		eMsg := ""
+		if err != nil {
+			eMsg = err.Error()
+		}
+
+		if eMsg != test.Err {
+			t.Errorf("Err = '%s', want '%s'", eMsg, test.Err)
+		}
+	}
+}
